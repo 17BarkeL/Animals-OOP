1151982a25a22e210bd08e6bca573fdd4f8e4ec3
Luke Barkess <luke.barkess@icloud.com>
2022-10-10 14:35:49 +0100

    Add project files.

---
+++ Animals OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog john = new Dog();
+            john.Fetch();
+            john.Eat();
+        }
+    }
+}
